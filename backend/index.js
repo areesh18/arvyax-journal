5a2cdc96a3b40ae0aeee47cc5c93c5d6eeb4381b
Areesh18 <areeshzafar396@gmail.com>
2026-03-14 18:38:01 +0530

    backend complete- all routes wokring

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import journalRoutes from "./routes/journal.js";
 import pool from "./db/index.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/journal", journalRoutes);
