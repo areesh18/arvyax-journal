06906b60b215c3409babebb056531167da691db9
Areesh18 <areeshzafar396@gmail.com>
2026-03-14 17:04:34 +0530

    C:/Program Files/Git/get route tested and working

--- backend/routes/journal.js
+++ backend/routes/journal.js
@@ -6,14 +6,22 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   const { userId, ambience, text } = req.body;
   const result = await pool.query(
-        `INSERT INTO journal_entries (user_id, ambience, text) 
+    `INSERT INTO journal_entries (user_id, ambience, text) 
          VALUES ($1, $2, $3) 
          RETURNING *`,
-        [userId, ambience, text]
-    )
+    [userId, ambience, text],
+  );
 
-
-    res.json(result.rows[0])
+  res.json(result.rows[0]);
 });
-
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const result = await pool.query(
+    `SELECT * FROM journal_entries
+    WHERE user_id = $1
+    ORDER BY created_at DESC`,
+    [userId],
+  );
+  res.json(result.rows);
+});
 export default router;
