c98ee576587a72e3a2cc3deca6978a6bf5e42228
Areesh18 <areeshzafar396@gmail.com>
2026-03-14 17:00:10 +0530

    C:/Program Files/Git/ route created and tested- working

--- backend/routes/journal.js
+++ backend/routes/journal.js
@@ -3,4 +3,17 @@ import pool from "../db/index.js";
 
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  const { userId, ambience, text } = req.body;
+  const result = await pool.query(
+        `INSERT INTO journal_entries (user_id, ambience, text) 
+         VALUES ($1, $2, $3) 
+         RETURNING *`,
+        [userId, ambience, text]
+    )
+
+
+    res.json(result.rows[0])
+});
+
 export default router;
